3660071e23b72654b086739711b6e07262ad1997
Shubham Kumar <shubham35godda@gmail.com>
2022-04-11 01:07:37 +0530

    created first page

---
+++ app/src/components/FirstPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../App.css"
+const FirstPage = () => {
+  return (
+      <>
+    <div className='center'>
+  <form>
+  
+  <div className='centerr'>  <label>  Name:</label>
+  <input type="text"></input></div>
+  <br></br>
+  <div className='centerr'>  <label>  Age:</label>
+  <input type="text"></input></div>
+  <br></br>
+  <div className='centerr'>  <label>  Dob:</label>
+  <input type="text"></input></div>
+  <diV className='centerr'><button className='btn'>Click Me</button></diV>
+  
+
+</form>
+</div>
+</>
+  )
+}
+
+export default FirstPage
