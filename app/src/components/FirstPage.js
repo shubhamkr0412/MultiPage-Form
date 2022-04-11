89e5a837799a976b14eb10dd1979f9f3ceb5c7cd
Shubham Kumar <shubham35godda@gmail.com>
2022-04-11 08:10:59 +0530

    created Json server

--- app/src/components/FirstPage.js
+++ app/src/components/FirstPage.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import "../App.css"
+import React from "react";
+import "../App.css";
+import { useState } from "react";
 const FirstPage = () => {
+  const [text, setText] = useState("");
+  // const [todos,setTodos]=useState([]);
   return (
-      <>
-    <div className='center'>
-  <form>
-  
-  <div className='centerr'>  <label>  Name:</label>
-  <input type="text"></input></div>
-  <br></br>
-  <div className='centerr'>  <label>  Age:</label>
-  <input type="text"></input></div>
-  <br></br>
-  <div className='centerr'>  <label>  Dob:</label>
-  <input type="text"></input></div>
-  <diV className='centerr'><button className='btn'>Click Me</button></diV>
-  
-
-</form>
-</div>
-</>
-  )
+    <>
+      <input
+        type="text"
+        
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter text"
+      />
+      <button onClick={()=>{
+       const payload={
+         title:text,
+         status:false,
+       };
+       fetch("http://localhost:3001/todos",{
+         method:"POST",
+         body:JSON.stringify(payload),
+         header:{
+           "content-type":"application/json",
+         },
+       })
+      }}>Submit</button>
+      {[].map((e)=>(<div>{e}</div>))}
+    </>
+  );
 }
 
-export default FirstPage
+export default FirstPage;
---
+++ app/src/components/a.js
@@ -0,0 +1,16 @@
+{/* <div className='center'>
+  <form>
+  
+  <div className='centerr'>  <label>  Name:</label>
+  <input type="text"></input></div>
+  <br></br>
+  <div className='centerr'>  <label>  Age:</label>
+  <input type="text"></input></div>
+  <br></br>
+  <div className='centerr'>  <label>  Dob:</label>
+  <input type="text"></input></div>
+  <diV className='centerr'><button className='btn'>Click Me</button></diV>
+  
+
+</form>
+</div> */}
