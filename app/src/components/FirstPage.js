bcd44032330e71dbb5cfb7c168cf60293fce325b
Shubham Kumar <shubham35godda@gmail.com>
2022-04-11 09:32:34 +0530

    Created backend  json server for first page

--- app/src/components/FirstPage.js
+++ app/src/components/FirstPage.js
@@ -2,21 +2,64 @@ import React from "react";
 import "../App.css";
 import { useState } from "react";
 const FirstPage = () => {
-  const [text, setText] = useState("");
-  console.log(text);
+  const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  console.log(age);
+  console.log(name);
+  console.log(date);
+
   // const [todos,setTodos]=useState([]);
   return (
-    <div>
-      <input
-        type="text"
-        
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Enter text"
-      />
-      <button onClick={()=>{
+    // <div>
+    //   <input
+    //     type="text"
+
+    //     onChange={(e) => setText(e.target.value)}
+    //     placeholder="Enter text"
+    //   />
+    //   <button onClick={()=>{
+    //    const payload={
+    //     title:text,
+    //     status:false
+    //    }
+    //    fetch("http://localhost:3001/todos",{
+    //      method:"POST",
+    //      body:JSON.stringify(payload),
+    //      headers:{
+    //        "content-type":"application/json",
+    //      },
+    //    });
+    //   }}>Submit</button>
+    //   {[].map((e)=>(<div>{e}</div>))}
+    // </div>
+
+    <div className="center">
+      <form>
+        <div className="centerr">
+          {" "}
+          <label> Name:</label>
+          <input type="text" onChange={(e) => setName(e.target.value)}></input>
+        </div>
+        <br></br>
+        <div className="centerr">
+          {" "}
+          <label> Age:</label>
+          <input type="number" onChange={(e) => setAge(e.target.value)}></input>
+        </div>
+        <br></br>
+        <div className="centerr">
+          {" "}
+          <label> Dob:</label>
+          <input type="date" onChange={(e) => setDate(e.target.value)}></input>
+        </div>
+        <div className="centerr">
+          <button className="btn" onClick={()=>{
        const payload={
-        title:text,
-        status:false
+        name:name,
+        age:age,
+        date:date,
+        required:true
        }
        fetch("http://localhost:3001/todos",{
          method:"POST",
@@ -25,10 +68,11 @@ const FirstPage = () => {
            "content-type":"application/json",
          },
        });
-      }}>Submit</button>
-      {[].map((e)=>(<div>{e}</div>))}
+      }}>Click Me</button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default FirstPage;
